0deb646dc537e36c2356c12238435bf8f5e1838a
sandhya-codes <gunduboinasandhya2004@gmail.com>
2025-03-03 21:15:46 +0530

    Add files via upload

---
+++ ps.js/first50pro.js
@@ -0,0 +1,43 @@
+// print hello world
+// console.log("hello world!")
+
+
+// add two numbers
+// let num1=1;
+// let num2=2;
+// console.log(num1+num2)
+
+
+// find the square 
+// function squareRoot(num,callback){
+//    console.log(callback(num))
+// }
+// var res= squareRoot(3,(a)=>{
+//   return a*a
+// })
+
+
+
+// area of triangle
+// function Triangle(len,bre,he,callback){
+//     console.log(callback(len,bre,he))
+    
+// }
+// console.log(Triangle(2,5,10,(a,b,c)=>{
+//     return a*b*c  
+// }))
+
+
+// swap two number 
+// num1=4
+// num2=2
+// temp=0
+// console.log("before swapping:",num1,num2)
+// temp=num1
+// num1=num2
+// num2=temp
+// console.log("after swapping:",num1,num2)
+
+ 
+// solve quadratic equation
+
---
+++ task3.js
@@ -0,0 +1,51 @@
+// Write a for loop to print numbers from 1 to 10.
+// for(i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+
+// Write a for loop to calculate the sum of all numbers from 1 to 100.
+// sum=0
+// for(i=1;i<=100;i++){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+// Write a for loop to print the multiplication table of 7 (up to 10).
+// num=7
+// for(i=1;i<=10;i++){
+//     console.log(`${num} X ${i} = ${num*i}`)
+// }
+
+
+// Write a for loop to print all even numbers between 1 and 20.
+// for(i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+ 
+
+
+// Factorial Calculation
+// let sum=1
+// for(let i=1;i<=5;i++){
+//      sum *=i
+// }
+// console.log(sum)
+
+
+// Write a for loop to calculate the power of a number (e.g., 2^5).
+// sum=1
+// for(i=1;i<=5;i++){
+//     sum=sum*i
+// }
+// console.log('2^5 is '+sum)
+
+
+// Write a for loop to print all odd numbers between 20 and 1 in reverse order.
+// for(let i=1;i<=20;i++){
+//     if(i%2!==0){
+//         console.log(i)
+//     }
+// }
