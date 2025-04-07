e35e23dea9f9ae96fb4c449f7e64f61053dbe732
sandhya-codes <gunduboinasandhya2004@gmail.com>
2025-04-07 21:23:04 +0530

    Add files via upload

---
+++ stringmethods.js
@@ -0,0 +1,41 @@
+// slice
+str1 = "sandhya"
+console.log(str1.slice(8))
+
+//substring()
+str1 = "sandhya"
+console.log(str1.substring(8))
+
+//substr()
+str1 = "sandhya"
+console.log(str1.substr(8))
+
+
+//replace()
+str1 ="sandhya"
+console.log(str1.replace("sandhya","shashi"))
+
+
+// replaceAll()
+str1 = " sandhya "
+console.log(str1.replaceAll("sandhya","dxfghjk"))
+
+
+console.log(str1.length)
+
+// toupper/tolower
+console.log(str1.toUpperCase())
+console.log(str1.toLowerCase())
+
+// includes
+console.log(str1.includes("sa"))
+
+console.log(str1.startsWith("s"))
+console.log(str1.endsWith("a"))
+console.log(str1.indexOf("h"))
+console.log(str1.charAt(1))
+
+// console.log(str1.padstart(1,""))
+console.log(str1.charCodeAt("h"))
+console.log(str1.split())
+console.log(str1.repeat(4))
